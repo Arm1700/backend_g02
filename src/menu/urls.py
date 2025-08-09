90fb2139f7d5e4185856092556eace89993e5df9
Arm1700 <bukboks1@gmail.com>
2025-08-09 14:45:48 +0400

    add line

--- src/menu/urls.py
+++ src/menu/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('category/<int:pk>/', CategoryDetailView.as_view(), name='category_detail'),
     path('menu/<int:pk>/', MenuDetialView.as_view(), name='menu_detail'),
     path('contact/', ContactView.as_view(), name='contact'),
+    
 ]
